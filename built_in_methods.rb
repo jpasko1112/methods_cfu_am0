9bf72bf26557b2af35d6e5041527af45f666e151
Justin Pasko <jpasko1112@gmail.com>
2023-02-10 12:59:23 -0500

    Add examples of methods in built_in_methods.rb

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,29 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The "include questionmark method" is being called on the string object "Hello World".
+#Returns true if the (argumented) object is included in the given string.
+#The return value will be true.
 "Hello World".include?("Hello")
 
-
+# The "end with questionmark method" is being called on the string object "Hello World"
+# The "end with questionmark method" is used to check if the given string ends with the argumented object.
+# The return value is false.
 "Hello World".end_with?("Hello")
 
-
+# The "end with question mark method"  is being called on the string object "Hello World"
+# The "end with question mark method"  is used to check if the given string ends with the argumented object.
+# The return value is true.
 "Hello World".end_with?("rld")
 
-
+# The "even question mark method" is being called on the integer 12.
+# The "even question mark method" is used to check if the given integer is even.
+# The return value is true.
 12.even?
 
-
+# The "integer next" function is being called on the integer 18.
+# The "integer next" function return the immediate successor of the given integer.
+# The return value is 19.
 18.next
 
 
@@ -39,7 +49,17 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+# The `reverse` method is called on the food variable, which stores the string object "cheese".
+# The reverse method returns a new string with the characters from the string object reversed.
+# The p command prints the return value of "eseehC" to the console.
+food = "Cheese"
+p food.reverse
 
+# The `chars` method is called on the mountain variable, which stores the string object "Whitney".
+# The `chars` method returns an array of characters from the string object.
+# The p command prints the return value ["w", "h", "i", "t", "n", "e", "y"]
+mountain = "Whitney"
+p mountain.chars
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
@@ -47,10 +67,30 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# The "odd question mark" method is being call on integer 123.
+# The "odd question mark" method is used to check if the given integer is odd.
+# The return value is true.
+123.odd?
 
+# The "zero question mark" method is being call on integer 123.
+# The "zero question mark" method is used to check if the given integer is zero.
+# The return value is false.
+900.zero?
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The shuffle method is being called on the variables inside the foods array.
+# The shuffle method is used to return a new array with elements of self shuffled.
+# The return value is ["meat", "sauce", "cheese", "bread"].
+foods = ["cheese", "bread", "sauce", "meat"]
+foods.shuffle
+
+# The max method is being called on the integers inst the nums array.
+# The max method is used to return the maximum-valued element of self.
+# The return value is 547.
+nums = [1, 5, 10, 30, 99, 412, 547]
+nums.max
